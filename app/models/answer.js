c4b1847be34036d1a4bf04e0d91f71a76d722d99
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-03-24 13:00:10 -0700

    add the proper answer functionality, correctly linking to specific answers

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+    author: DS.attr(),
     body: DS.attr(),
-    title: DS.attr(),
-    answers: DS.hasMany('answer', {async: true})
+    question: DS.belongsTo('question', {async:true})
 });
